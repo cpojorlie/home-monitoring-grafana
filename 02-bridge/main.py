938bd3410d8640ca6f80c2a5deb3e17add2d6eb5
Clayton <clayton.pojorlie@gmail.com>
2021-04-08 20:58:50 -0600

    added .gitignore update main.py to tasmota parsing

--- 02-bridge/main.py
+++ 02-bridge/main.py
@@ -6,31 +6,36 @@ This script receives MQTT data and saves those to InfluxDB.
 
 """
 
+import datetime
 import re
+import time
 from typing import NamedTuple
+import json
 
 import paho.mqtt.client as mqtt
 from influxdb import InfluxDBClient
 
 INFLUXDB_ADDRESS = 'influxdb'
-INFLUXDB_USER = 'root'
-INFLUXDB_PASSWORD = 'root'
-INFLUXDB_DATABASE = 'home_db'
+INFLUXDB_USER = 'telegraf'
+INFLUXDB_PASSWORD = 'telegraf'
+INFLUXDB_DATABASE = 'sensors'
 
 MQTT_ADDRESS = 'mosquitto'
-MQTT_USER = 'mqttuser'
-MQTT_PASSWORD = 'mqttpassword'
-MQTT_TOPIC = 'home/+/+'  # [bme280|mijia]/[temperature|humidity|battery|status]
-MQTT_REGEX = 'home/([^/]+)/([^/]+)'
+MQTT_USER = 'mqtt_user'
+MQTT_PASSWORD = 'mqtt_pass'
+#MQTT_TOPIC = 'home/+/+'  # [bme280|mijia]/[temperature|humidity|battery|status]
+MQTT_TOPIC = 'sensors/tele/+/SENSOR'
+MQTT_REGEX = 'sensors/tele/([^/]+)/SENSOR'
 MQTT_CLIENT_ID = 'MQTTInfluxDBBridge'
 
 influxdb_client = InfluxDBClient(INFLUXDB_ADDRESS, 8086, INFLUXDB_USER, INFLUXDB_PASSWORD, None)
 
 
 class SensorData(NamedTuple):
-    location: str
     measurement: str
-    value: float
+    tags: dict
+    fields: dict
+    timestamp: int
 
 
 def on_connect(client, userdata, flags, rc):
@@ -42,19 +47,37 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     """The callback for when a PUBLISH message is received from the server."""
     print(msg.topic + ' ' + str(msg.payload))
-    sensor_data = _parse_mqtt_message(msg.topic, msg.payload.decode('utf-8'))
-    if sensor_data is not None:
-        _send_sensor_data_to_influxdb(sensor_data)
+    _parse_mqtt_message(msg.topic, json.loads(msg.payload))       
 
 
 def _parse_mqtt_message(topic, payload):
+    tags={}
+    fields={}
+    sensors=[]
     match = re.match(MQTT_REGEX, topic)
     if match:
-        location = match.group(1)
-        measurement = match.group(2)
-        if measurement == 'status':
-            return None
-        return SensorData(location, measurement, float(payload))
+        tags['location'] = match.group(1)
+        #measurement = match.group(2)
+
+        for item in payload:
+            if item == 'Time':
+                timestamp = int(time.mktime(datetime.datetime.strptime(payload[item],'%Y-%m-%dT%H:%M:%S').timetuple()))
+            elif type(payload[item]) is dict:
+                sensors.append(item)
+            else:
+                tags[item]= payload[item]
+        
+        for s in sensors:
+            tags['sensor'] = s
+            for item in payload[s]:
+                if item == 'Id':
+                    tags[item]= payload[s][item]
+                else:
+                    measurement = item
+                    fields['value'] = float(payload[s][item])
+            sensor_data = SensorData(measurement, tags, fields, timestamp)
+            _send_sensor_data_to_influxdb(sensor_data)
+            print(sensor_data)
     else:
         return None
 
@@ -63,15 +86,13 @@ def _send_sensor_data_to_influxdb(sensor_data):
     json_body = [
         {
             'measurement': sensor_data.measurement,
-            'tags': {
-                'location': sensor_data.location
-            },
-            'fields': {
-                'value': sensor_data.value
-            }
+            'tags': sensor_data.tags,
+            'fields': sensor_data.fields,
+            'timestamp': sensor_data.timestamp
         }
     ]
-    influxdb_client.write_points(json_body)
+    influxdb_client.write_points(json_body, time_precision='s')
+    #print(json_body)
 
 
 def _init_influxdb_database():
